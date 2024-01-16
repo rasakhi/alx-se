835862b704be5ec6e6c38b8028f8b9c38cfd08f0
Michael Achumba <michaelachumba@gmail.com>
2024-01-16 20:28:27 +0100

    This is my alx-se personal study repository

---
+++ c-programming/0x00/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string with puts function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ c-programming/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints hello world
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	printf("Hello, world!\n");
+	return (0);
+}
